14ec22f9672964f5198ace170a7df649cb9cbd9e
Lisa Wells <lisawells999@gmail.com>
2016-05-21 07:52:42 -0400

    consolidate all the enqueues into one function

--- functions.php
+++ functions.php
@@ -3,18 +3,20 @@
  *
  */
 
-// including parent theme css
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
-function enqueue_parent_styles() {
-   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-}
+add_action( 'wp_enqueue_scripts', 'ch_enqueue_changehappens' );
+function ch_enqueue_changehappens() {
 
-// including changehappens js
-add_action( 'wp_enqueue_scripts', 'enqueue_changehappens_js' );
-function enqueue_changehappens_js() {
+    // including parent theme css
+    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+
+    // including changehappens js
     if (is_page('thank-you-guide')) {
         wp_enqueue_script( 'changehappens_js', get_stylesheet_directory_uri() . '/js/changehappenspdf.js', array(), true );
     }
+
+    // use my version of sticky.js instead
+    wp_dequeue_script( 'hello-world-sticky-kit', get_template_directory_uri() . '/js/sticky-kit.js', array( 'jquery' ), '20150128', true );
+    wp_enqueue_script('change-happens-sticky-kit', get_stylesheet_directory_uri().'/js/sticky-kit.js', array('jquery'), '20150128', true);
 }
 
 // including child theme template-tags
@@ -24,19 +26,4 @@ include( get_stylesheet_directory() . '/inc/template-tags.php' );
 add_action( 'after_setup_theme', 'ch_theme_thumb', 11 );
 function ch_theme_thumb() {
     set_post_thumbnail_size( 300 );
-}
-
-// use my version of sticky.js instead
-// here's the parent's
-        // wp_enqueue_script( 'hello-world-sticky-kit-js', get_template_directory_uri() . '/js/jquery.sticky-kit.min.js', array( 'jquery' ), '1.0.4' );
-
-// http://wordpress.stackexchange.com/questions/26822/how-to-override-javascript-files-in-child-theme
-// hook in late to make sure the parent theme's registration
-// has fired so you can undo it. Otherwise the parent will simply
-// enqueue its script anyway.
-add_action('wp_enqueue_scripts', 'wpse26822_script_fix', 100);
-function wpse26822_script_fix()
-{
-    wp_dequeue_script( 'hello-world-sticky-kit', get_template_directory_uri() . '/js/sticky-kit.js', array( 'jquery' ), '20150128', true );
-    wp_enqueue_script('change-happens-sticky-kit', get_stylesheet_directory_uri().'/js/sticky-kit.js', array('jquery'), '20150128', true);
 }
