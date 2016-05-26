2d7acdb96e4c0754af0f3607df557b366beb13a9
Lisa Wells <lisawells999@gmail.com>
2016-05-26 09:06:34 -0400

    load real pdf; take away space on thank-you pages

--- js/changehappenspdf.js
+++ js/changehappenspdf.js
@@ -1,1 +1,1 @@
-        setTimeout('window.open("http://changehappens.dev/wp-content/uploads/2016/05/ChangeHappensMock.pdf", "_blank")', 5000);
+        setTimeout('window.open("http://77.104.139.80/~changeh8/wp-content/uploads/2016/05/A-Guide-to-Creating.pdf", "_blank")', 5000);
